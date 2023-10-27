cd6abed9ceb69e4e3788c618e0a074492602fde6
John Smith <example@email.com>
2023-10-27 12:35:11 +0700

    1

--- src/app/admin/color/components/color-dialog/color-dialog.component.ts
+++ src/app/admin/color/components/color-dialog/color-dialog.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import { MessageService } from 'primeng/api';
+import { ColorService } from '../../service/color.service';
 
 @Component({
   selector: 'app-color-dialog',
   templateUrl: './color-dialog.component.html',
-  styleUrls: ['./color-dialog.component.scss']
+  styleUrls: ['./color-dialog.component.scss'],
 })
 export class ColorDialogComponent {
-
+  color: any = {};
+  type: any;
+  ngOnInit(): void {}
+  constructor(
+    // @Inject(MAT_DIALOG_DATA) public data: any,
+    private colorService: ColorService,
+    private dialog: MatDialog
+  ) // private messageService: MessageService
+  {
+    // this.type = data.type;
+    // this.color = data.color;
+  }
+  addColor() {
+    this.colorService.createColor(this.color).then((res) => {
+      if (res) {
+        this.dialog.closeAll();
+        // this.messageService.add({
+        //   severity: 'success',
+        //   summary: 'Success',
+        //   detail: 'add thành công',
+        // });
+      } else {
+        // this.messageService.add({
+        //   severity: 'error',
+        //   summary: 'Error',
+        //   detail: 'add thất bại',
+        // });
+      }
+    });
+  }
+  updateColor() {
+    this.colorService.updateColor(this.color, this.color.id).then((res) => {
+      if (res) {
+        this.dialog.closeAll();
+      }
+    });
+  }
+  deleteColor() {
+    this.colorService.deleteColor(this.color.id);
+  }
 }
--- src/app/admin/color/service/color.service.ts
+++ src/app/admin/color/service/color.service.ts
@@ -1,20 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IReqApi } from 'src/libs/common/interface/interfaces';
 import { IColor } from './color.module';
-import { BaseRequestService } from '../../../../libs/service/request/base-request.service';
+import { BaseRequestService } from 'src/libs/service/request/base-request.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ProductService {
+export class ColorService {
   url: string = 'color';
 
-  constructor(
-    private BaseRequestService: BaseRequestService
-  ) { }
-  getColors(): Promise<IColor[]> {
-    return new Promise<IColor[]>((resolve, reject) => {
-      this.BaseRequestService.get(`${this.url}`).subscribe(
+  constructor(private baseRequestService: BaseRequestService) {}
+  getColors(params?: any): Promise<IReqApi<IColor[]>> {
+    return new Promise<IReqApi<IColor[]>>((resolve, reject) => {
+      this.baseRequestService.get(`${this.url}`, params).subscribe(
         (result) => {
           return resolve(result);
         },
@@ -22,13 +21,30 @@ export class ProductService {
       );
     });
   }
-
-  getColorByName(name: string): Promise<IColor> {
-    return new Promise<IColor>((resolve, reject) => {
-      this.BaseRequestService.get(`${this.url}/${name}`).subscribe(
+  createColor(body: any): Promise<IReqApi<IColor[]>> {
+    return new Promise<IReqApi<IColor[]>>((resolve, reject) => {
+      this.baseRequestService.post(`${this.url}`, body).subscribe(
         (result) => {
           return resolve(result);
         },
+        (err) => reject(err)
+      );
+    });
+  }
+  updateColor(body: any, id?: any): Promise<IReqApi<IColor[]>> {
+    return new Promise<IReqApi<IColor[]>>((resolve, reject) => {
+      this.baseRequestService.put(`${this.url}/${id}`, body).subscribe(
+        (result) => {
+          return resolve(result);
+        },
+        (err) => reject(err)
+      );
+    });
+  }
+  deleteColor(id: any): Promise<IReqApi<IColor[]>> {
+    return new Promise<IReqApi<IColor[]>>((resolve, reject) => {
+      this.baseRequestService.delete(`${this.url}/${id}`).subscribe(
+        (result) => {},
         (err) => reject(err)
       );
     });
