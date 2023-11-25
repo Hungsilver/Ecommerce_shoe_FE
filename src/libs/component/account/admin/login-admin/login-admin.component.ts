2e3e0ec478b1d7e47fbcb3a8acbbc0b7a3793771
Hưng Silver <hungsilvertq@gmail.com>
2023-11-25 18:20:27 +0700

    ok

--- src/libs/component/account/admin/login-admin/login-admin.component.ts
+++ src/libs/component/account/admin/login-admin/login-admin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NotificationService } from 'src/libs/service/notification/notification.service';
 import { BaseAuthService } from 'src/libs/service/request/auth.service';
 import { CacheService } from 'src/libs/service/request/cache.service';
 
@@ -12,9 +14,10 @@ export class LoginAdminComponent implements OnInit {
   userLogin!: any;
   constructor(
     private formBuilder: FormBuilder,
+    private router: Router,
     private cacheService: CacheService,
     private authService: BaseAuthService,
-
+    // private notificationService: NotificationService
   ) { }
   formLogin: FormGroup = this.formBuilder.group({
     email: ['', [Validators.required, Validators.email]],
@@ -22,16 +25,30 @@ export class LoginAdminComponent implements OnInit {
   });
 
   onSubmit() {
-    if (this.formLogin.valid) {
-      // alert(JSON.stringify(this.formLogin.value));
-      // this.cacheService.
-      this.authService.authenticateAdmin(this.formLogin.value).subscribe(async (res: any) => {
-        this.cacheService.set('ADMIN', res.content);
-      }, err => {
-        console.log(err);
-      }, () => { }
-      )
 
+    if (this.formLogin.valid) {
+      //   // alert(JSON.stringify(this.formLogin.value));
+      //   // this.cacheService.
+      //   this.authService.authenticateAdmin(this.formLogin.value).subscribe(async (res: any) => {
+      //     if (res?.content) {
+      //       this.cacheService.set('admin', res.content);
+      //     }
+      //   }, err => {
+      //     console.log(err);
+      //   }, () => { }
+      //   )
+      //call api luu vao cache
+      if (this.formLogin.get('email')?.value === 'admin@gmail.com'
+        && this.formLogin.get('password')?.value === 'admin@123'
+      ) {
+        this.cacheService.set('admin', { id: 1, ten: 'hung silver' });
+        this.router.navigateByUrl('/admin')
+      }
+
+      // if (!this.cacheService.get('admin')) {
+      //   alert('loi login admin');
+      // } else {
+      // }
 
     }
   }
