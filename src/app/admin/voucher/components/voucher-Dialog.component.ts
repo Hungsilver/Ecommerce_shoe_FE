069131c0ec25850cefd12d91f0be719fc3acdc41
chien210030 <chien210030@gmail.com>
2023-12-26 15:11:26 +0700

    voucher còn validate

--- src/app/admin/voucher/components/voucher-Dialog.component.ts
+++ src/app/admin/voucher/components/voucher-Dialog.component.ts
@@ -1,8 +1,9 @@
 import { Component, Inject, OnInit } from "@angular/core";
 import { VoucherSevice } from "../service/voucher.service";
 import { MAT_DIALOG_DATA,MatDialog } from "@angular/material/dialog";
-// import * as moment from 'moment';
-// import { utcToZonedTime } from 'date-fns-tz';
+import { Moment } from "moment";
+import * as moment from "moment";
+// import * as moment from "moment";
 
 
 @Component({
@@ -32,18 +33,35 @@ type: any;
   }
 
 addVoucher(){
-  console.log('time chua set:', this.voucher.thoiGianKetThuc);
+    const thoiGianHienTai : Moment = moment();
+      console.log('time now:', thoiGianHienTai);
+      this.voucher.thoiGianBatDau = moment(this.voucher.thoiGianBatDau).toISOString();
+      this.voucher.thoiGianKetThuc = moment(this.voucher.thoiGianKetThuc).toISOString();
+    console.log('time start:',this.voucher.thoiGianBatDau);
+  //   const timestampBatDau = moment(this.voucher.thoiGianBatDau).unix();
+  // const timestampKetThuc = moment(this.voucher.thoiGianKetThuc).unix();
+if(this.voucher.thoiGianBatDau ==='' || this.voucher.thoiGianKetThuc ===''){
+  // điều kiện này chưa đc nên check lại
+    alert('Không Được để trống');
+}else  if( this.voucher.thoiGianBatDau >= this.voucher.thoiGianKetThuc){
+    console.log('time end:',this.voucher.thoiGianKetThuc);
+    alert('Thời gian Bắt đầu không được lớn hoặc bằng Thời gian kết thúc');
 
+  }else if(thoiGianHienTai.isBefore(this.voucher.thoiGianBatDau)){
 
-  this.voucher.thoiGianKetThuc = new Date(this.voucher.thoiGianKetThuc).toISOString();
-  console.log('tim da set:',this.voucher.thoiGianKetThuc);
-this.voucherService.createVoucher(this.voucher).then((res) => {
-  console.log('data created', res.content);
-  if (res) {
+      this.voucherService.createVoucher(this.voucher).then((res) => {
+      console.log('data created', res.content);
+   if (res) {
     this.dialog.closeAll();
-  }
+    }
 });
+
+} else{
+  alert('Thời gian bắt đầu không được nhỏ hơn thời gian hiện tại. Vui lòng nhập lại.');
 }
+
+}
+
 updateVoucher(){
   this.voucherService.updateVoucher(this.voucher, this.voucher.id).then((res) => {
     if (res) {
