82362a370e18b0b619e6fdcc6d11081ac7926737
chien210030 <chien210030@gmail.com>
2023-12-30 00:25:50 +0700

    no mess

--- src/app/admin/product/service/product.module.ts
+++ src/app/admin/product/service/product.module.ts
@@ -1,13 +1,13 @@
-export interface IProduct {
-  id?: number;
-  sanpham?: string;
-  anhSanPham?: string;
-  soLuong?: string;
-  giaBan?: number;
-  ngayTao?: string;
-  trangThai?: string;
-  mausac?: string;
-  kichco?: string;
-  chatLieuGiay?: string;
-  chatLieuDeGiay?: string;
-}
+// export interface IProduct {
+//   id?: number;
+//   sanpham?: string;
+//   anhSanPham?: string;
+//   soLuong?: string;
+//   giaBan?: number;
+//   ngayTao?: string;
+//   trangThai?: string;
+//   mausac?: string;
+//   kichco?: string;
+//   chatLieuGiay?: string;
+//   chatLieuDeGiay?: string;
+// }
